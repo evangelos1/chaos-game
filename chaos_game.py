3b13ff4f672ae41f5f1cf0c842e885f6a46d4eca
Evan <32958987+evangelos1@users.noreply.github.com>
2019-08-11 02:52:43 +0200

    Update chaos_game.py

--- chaos_game.py
+++ chaos_game.py
@@ -6,23 +6,28 @@ Created on Wed May 29 16:34:01 2019
 @author: evangelos
 """
 
-# The chaos game
+# The chaos game. See wikipedia.org/wiki/Chaos_game for more information.
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Set the coordinates of the vertices
 p1 = (0, 0)
 p2 = (10, 0)
 p3 = (5, np.sqrt(75))
 
+# Choose the number of iterations
 iterations = 100000
 
+# This array will be used to save the coordinates of the cursor after every iteration
 points = np.zeros((iterations, 2))
 
+# Set the starting point
 points[0, :] = (np.random.randint(0, 10+1), np.random.randint(0, 10+1))
 #points[0, :] = (0, 0)
 #points[0, :] = (5, 2.5)
 
+# Play!
 for i in np.arange(1, iterations):
     throw = np.random.randint(1, 3+1)
     if throw==1:
@@ -34,9 +39,8 @@ for i in np.arange(1, iterations):
     elif throw==3:
         points[i, 0] = (p3[0] + points[i-1, 0]) / 2
         points[i, 1] = (p3[1] + points[i-1, 1]) / 2
-    else:
-        print('throw not in [1, 3]')
 
+# Plot the result
 plt.figure(10)
 plt.clf()
 plt.plot(points[:, 0], points[:, 1], 'bv', ms=0.5)
