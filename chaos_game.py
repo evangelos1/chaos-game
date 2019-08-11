e60c14a309767245c9d7314bd84a70d7b36c1aad
Evan <32958987+evangelos1@users.noreply.github.com>
2019-08-11 02:43:13 +0200

    source code

---
+++ chaos_game.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 16:34:01 2019
+
+@author: evangelos
+"""
+
+# The chaos game
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+p1 = (0, 0)
+p2 = (10, 0)
+p3 = (5, np.sqrt(75))
+
+iterations = 100000
+
+points = np.zeros((iterations, 2))
+
+points[0, :] = (np.random.randint(0, 10+1), np.random.randint(0, 10+1))
+#points[0, :] = (0, 0)
+#points[0, :] = (5, 2.5)
+
+for i in np.arange(1, iterations):
+    throw = np.random.randint(1, 3+1)
+    if throw==1:
+        points[i, 0] = (p1[0] + points[i-1, 0]) / 2
+        points[i, 1] = (p1[1] + points[i-1, 1]) / 2
+    elif throw==2:
+        points[i, 0] = (p2[0] + points[i-1, 0]) / 2
+        points[i, 1] = (p2[1] + points[i-1, 1]) / 2
+    elif throw==3:
+        points[i, 0] = (p3[0] + points[i-1, 0]) / 2
+        points[i, 1] = (p3[1] + points[i-1, 1]) / 2
+    else:
+        print('throw not in [1, 3]')
+
+plt.figure(10)
+plt.clf()
+plt.plot(points[:, 0], points[:, 1], 'bv', ms=0.5)
+plt.plot(points[0, 0], points[0, 1], 'r+')
